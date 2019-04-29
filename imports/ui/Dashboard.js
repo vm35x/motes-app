0fb74a8ff0c8501c30c91812bf07273f942c6ade
Vladimir Mezentsov <vmezentsov@InComm.com>
2019-04-29 09:10:31 -0400

    setup mobile navigation

--- imports/ui/Dashboard.js
+++ imports/ui/Dashboard.js
@@ -7,7 +7,7 @@ import Editor from "./Editor";
 const Dashboard = () => {
   return (
     <div>
-      <PrivateHeader title="Dashboard" />
+      <PrivateHeader title="Notes" />
       <div className="page-content">
         <div className="page-content__sidebar">
           <NoteList />
--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -2,11 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Accounts } from "meteor/accounts-base";
 import { createContainer } from "meteor/react-meteor-data";
+import { Session } from "meteor/session";
 
 export const PrivateHeader = props => {
+  const navimageSrc = props.isNavOpen ? "/images/x.svg" : "/images/bars.svg";
+
   return (
     <div className="header">
       <div className="header__content ">
+        <img
+          onClick={props.handleNavToggle}
+          src={navimageSrc}
+          alt=""
+          className="header__nav-toggle"
+        />
         <h1 className="header__title">{props.title}</h1>
         <button
           className="button button--link-text"
@@ -21,12 +30,16 @@ export const PrivateHeader = props => {
 
 export default createContainer(() => {
   return {
-    handleLogout: () => Accounts.logout()
-  }
+    handleLogout: () => Accounts.logout(),
+    isNavOpen: Session.get("isNavOpen"),
+    handleNavToggle: () => Session.set("isNavOpen", !Session.get("isNavOpen"))
+  };
 }, PrivateHeader);
 // export default PrivateHeader;
 
 PrivateHeader.propTypes = {
   title: PropTypes.string.isRequired,
-  handleLogout: PropTypes.func.isRequired
+  handleLogout: PropTypes.func.isRequired,
+  handleNavToggle: PropTypes.func.isRequired,
+  isNavOpen: PropTypes.bool.isRequired
 };
